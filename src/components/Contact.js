b3cf8b86d3559eb8424a9d5a9622c0b0623ebf14
BranHill21 <bhill20@austincollege.edu>
2024-08-06 13:46:02 -0500

    Setting up basic template

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+// src/components/Contact.js
+
+const Contact = () => {
+    return (
+      <div>
+        <h1>Contact Me</h1>
+        <p>contact info</p>
+      </div>
+    );
+  };
+  
+  export default Contact;
---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+// src/components/Skills.js
+
+const Skills = () => {
+  return (
+    <div>
+      <h1>These are my skills</h1>
+      <p>skills</p>
+    </div>
+  );
+};
+
+export default Skills;
