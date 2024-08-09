1be36938dc7472b0a97b4b0f114e5d63377b4586
BranHill21 <bhill20@austincollege.edu>
2024-08-09 01:49:13 -0500

    removed redundancy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Navbar from './components/Navbar';
 import './Styles/Styles.css';
 
 const App = () => {
